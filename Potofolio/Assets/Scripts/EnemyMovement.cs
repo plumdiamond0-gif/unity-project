7dcc421871684f13b9049252519ff3a151a9f61c
plumdiamond0-gif <plumdiamond0@gmail.com>
2026-04-28 02:03:20 +0900

    4_28
    에이전트 넉백 문제 해결 필요

--- Potofolio/Assets/Scripts/EnemyMovement.cs
+++ Potofolio/Assets/Scripts/EnemyMovement.cs
@@ -1,14 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem.Utilities;
 using static UnityEngine.GraphicsBuffer;
 
+public enum EnemyState
+{
+    Idle,
+    Chase,
+    Attack,
+    Knockback,
+    Stun
+}
 public class EnemyMovement : MonoBehaviour
 {
-    public NavMeshAgent agent;
-    public Transform player;
-    public float detectRange = 26f;
+    public EnemyState currentState;
+
+    public GameObject player;
+    Transform playerTrans;
+    public float detectRange = 10f;
+    public float attackRange = 2f;
+
+    private NavMeshAgent agent;
+    private Rigidbody rb;
+
+    private bool isKnockbacking = false;
+
     public float damage;
     static readonly int MoveHash = Animator.StringToHash("Move");
     static readonly int AttackHash = Animator.StringToHash("Attack");
@@ -19,39 +39,128 @@ public class EnemyMovement : MonoBehaviour
 
     Animator anim;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
+
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        playerTrans = player.transform;
+        rb.isKinematic = true; // 평소엔 물리 OFF
         anim = GetComponentInChildren<Animator>();
 
         anim.SetFloat(MoveHash, 0);
-        agent = GetComponent<NavMeshAgent>();
 
         agent.isStopped = true;
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        if (agent != null && player != null)
-        {
-            agent.SetDestination(player.position);
-        }
+
+     
+        if (currentState == EnemyState.Knockback || currentState == EnemyState.Stun)
+            return;
+
+        float dist = Vector3.Distance(transform.position, playerTrans.position);
+
+        if (dist > detectRange)
+            ChangeState(EnemyState.Idle);
+        else if (dist > attackRange)
+            ChangeState(EnemyState.Chase);
+        else
+            ChangeState(EnemyState.Attack);
+
+        HandleState();
 
 
-        if (Vector3.Distance(transform.position, player.position) <= detectRange)
+
+        /*if (agent != null && player != null)
+        //{
+        //    agent.SetDestination(player.position);
+        //}
+        매 프레임마다 계속 플레이어 위치 목적지로 삼아 코드가 매우 무거워짐*/
+
+    }
+
+    private void HandleState()
+    {
+        if(agent.enabled == false)
+            { return; }
+        switch (currentState)
         {
-            agent.isStopped = false;
-            anim.SetFloat(MoveHash, 1f);
+            case EnemyState.Idle:
+                if (isKnockbacking)
+                    return;
+                agent.ResetPath();
+                anim.SetFloat(MoveHash, 0);
+                break;
+            case EnemyState.Chase:
+                agent.SetDestination(playerTrans.position);
+                anim.SetFloat(MoveHash, 1);
+                break;
+            case EnemyState.Attack:
+                Health playerhealth = player.GetComponent<Health>();
+                if (playerhealth != null)
+                {
+                    anim.SetTrigger(AttackHash);
+                    playerhealth.TakeHealth(-damage);
+                }
+                agent.ResetPath();
+                break;
+
+            default:
+                break;
         }
-        
+
+    }
+
+    private void ChangeState(EnemyState newState)
+    {
+        if (currentState == newState)
+        { return; }
+        currentState = newState;
+
     }
 
     public void TakeDamage(float Getdamage)
     {
 
     }
-    private void OnTriggerStay(Collider other)
+
+    public void ApplyKnockBack(float knockbackTime)
+    {
+        if (isKnockbacking)
+            return;
+        StartCoroutine(KnockbackRoutine(knockbackTime));
+    }
+    IEnumerator KnockbackRoutine(float time)
+    {
+        isKnockbacking = true;
+        currentState = EnemyState.Knockback;
+        agent.enabled = false;  
+        rb.isKinematic = false;
+
+        yield return new WaitForSeconds(time);
+
+        rb.isKinematic = true;
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(transform.position, out hit, 200f, NavMesh.AllAreas))
+        {
+            agent.Warp(hit.position);
+        }
+
+        agent.enabled = true;
+
+
+        currentState = EnemyState.Chase;
+        isKnockbacking = false;
+
+
+
+    }
+    /*private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
@@ -64,7 +173,7 @@ public class EnemyMovement : MonoBehaviour
             }
         }
     }
-
+    */
 
 
 
--- Potofolio/Assets/Scripts/KnockBackEffect.cs
+++ Potofolio/Assets/Scripts/KnockBackEffect.cs
@@ -4,9 +4,11 @@ using UnityEngine.AI;
 [CreateAssetMenu(menuName = "Weapon/Effects/KnockBack")]
 public class KnockBackEffect : ScriptableObject, IWeaponEffect
 {
-    float radius = 5.0f;       // 폭발 반경
-    float force = 20.0f;       // 폭발 위력
-    float upModifier = 3.0f;   // 위로 띄우는 정도 (이게 있어야 시원하게 날아감)
+
+    [SerializeField] float radius = 5.0f;       // 폭발 반경
+    [SerializeField] float force = 20.0f;       // 폭발 위력
+    [SerializeField] float upModifier = 3.0f;   // 위로 띄우는 정도 (이게 있어야 시원하게 날아감)
+    [SerializeField] float knockbackTime;
 
     public void Apply(GameObject target)
     {
@@ -18,12 +20,11 @@ public class KnockBackEffect : ScriptableObject, IWeaponEffect
        
         foreach (Collider hit in colliders)
         {
-            Debug.Log("fff");
-            //NavMeshAgent agent = hit.GetComponent<NavMeshAgent>();
-            //agent.enabled = false;
-            // 2. 리지드바디가 있는 오브젝트만 골라냄
+            //if(!hit.gameObject.CompareTag("Enemy"))
+            //{
+            //    return;
+            //}
             Rigidbody rb = hit.GetComponent<Rigidbody>();
-
             if (rb != null)
             {
                 // 3. 폭발 물리 적용 (중심점에서 밖으로 날려버림)
@@ -31,6 +32,13 @@ public class KnockBackEffect : ScriptableObject, IWeaponEffect
 
                 Debug.Log("넉백");
             }
+            EnemyMovement enemy = hit.GetComponent<EnemyMovement>();
+            if (enemy != null)
+            {
+                enemy.ApplyKnockBack(force);
+            }
+
+
         }
     }
 }
